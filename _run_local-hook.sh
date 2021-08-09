599bb9234e1a2e313d20f3a6e578a06a75947d67
Satoshi Nishimoto <sitos.nishi@gmail.com>
2021-08-09 15:29:36 +0900

    :tada: Implement a new function to detect nbsp

--- _run_local-hook.sh
+++ _run_local-hook.sh
@@ -1,11 +1,32 @@
-#!/bin/bash
+#!/bin/zsh
 
 GIT_ROOT=`git rev-parse --show-superproject-working-tree --show-toplevel | head -1`
 HOOK_NAME=`basename $0`
 LOCAL_HOOK="${GIT_ROOT}/.git/hooks/${HOOK_NAME}"
+retcode=0
 
 if [ -e $LOCAL_HOOK ]; then
   source $LOCAL_HOOK
 fi
 
-echo "Hello!!"
+# Function
+function detect_no-break-space(){
+  file_arr=(
+    $( find . -type f -exec grep -Iq . {} \; -print | grep -v '.git/' )
+  )
+  for file in "${file_arr[@]}" ; do
+    ret=$(grep -nE '\xa0' "${file}")
+    if [ $? -eq 0 ] ; then
+      echo "WARN : NBSP Detected -> ${file}"
+      echo "------"
+      echo ${ret}
+      echo "------"
+      retcode=255
+    fi
+  done
+}
+
+# Main
+detect_no-break-space
+[ ${retcode} -eq 0 ] && echo "PROCEED : git commit" || echo "ABORT : git commit"
+exit ${retcode}
